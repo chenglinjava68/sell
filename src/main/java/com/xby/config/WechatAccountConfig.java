ae234c80f7e3e220d00a3109a9bb0d490c205287
summerbegins <2746370543@qq.com>
2019-06-26 15:27:43 +0800

    微信公众号相关信息

---
+++ src/main/java/com/xby/config/WechatAccountConfig.java
@@ -0,0 +1,42 @@
+package com.xby.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @author :xby
+ * @date :2019/2/4 15:50
+ * @description :微信公众号
+ */
+@Data
+@Component
+@ConfigurationProperties(prefix = "wechat")
+public class WechatAccountConfig {
+
+    //公众平台id
+    private String mpAppId;
+
+    //公众平台密钥
+    private String mpAppSecret;
+
+    //开放平台id
+    private String openAppId;
+
+    //开放平台密钥
+    private String openAppSecret;
+
+    //商户号
+    private String mchId;
+
+    //商户密钥
+    private String mchKey;
+
+    //商户证书路径
+    private String keyPath;
+
+    //微信支付异步通知地址
+    private String notifyUrl;
+}
