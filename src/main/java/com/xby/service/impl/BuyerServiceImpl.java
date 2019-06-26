ea729b6d4194e1502134ae2883dabdca810b35c2
summerbegins <2746370543@qq.com>
2019-06-26 15:47:35 +0800

    查询、取消订单

---
+++ src/main/java/com/xby/service/impl/BuyerServiceImpl.java
@@ -0,0 +1,64 @@
+package com.xby.service.impl;
+
+import com.xby.dto.OrderDTO;
+import com.xby.enums.ResultEnum;
+import com.xby.exception.SellException;
+import com.xby.service.BuyerService;
+import com.xby.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author :xby
+ * @date :2019/1/27 19:08
+ * @description :
+ */
+@Service
+@Slf4j
+public class BuyerServiceImpl implements BuyerService {
+
+    @Autowired
+    private OrderService orderService;
+
+    /**
+     * 根据orderId查询订单
+     * @param openid
+     * @param orderId
+     * @return
+     */
+    @Override
+    public OrderDTO findOrderOne(String openid, String orderId) {
+        return checkOrderOwner(openid,orderId);
+    }
+
+    /**
+     * 根据orderId取消订单
+     * @param openid
+     * @param orderId
+     * @return
+     */
+    @Override
+    public OrderDTO cancelOrder(String openid, String orderId) {
+        OrderDTO orderDTO = checkOrderOwner(openid, orderId);
+        if (orderDTO == null) {
+            log.error("【取消订单】查不到该订单, orderId={}", orderId);
+            throw new SellException(ResultEnum.ORDER_NOT_EXIST);
+        }
+        return orderService.cancel(orderDTO);
+    }
+
+
+    private OrderDTO checkOrderOwner(String openid, String orderId) {
+        OrderDTO orderDTO = orderService.findOne(orderId);
+        if (orderDTO == null) {
+            return null;
+        }
+        //查询是否是自己的订单
+        if (!orderDTO.getBuyerOpenid().equalsIgnoreCase(openid)) {
+            log.error("【查询订单】订单的openid不一致. openid={}, orderDTO={}", openid, orderDTO);
+            throw new SellException(ResultEnum.ORDER_OWNER_ERROR);
+        }
+        return orderDTO;
+    }
+}
