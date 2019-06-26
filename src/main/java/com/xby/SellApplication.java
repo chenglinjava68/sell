46a74031f81145fe619db7fbe815c0aed74bf8b5
summerbegins <2746370543@qq.com>
2019-06-26 14:39:08 +0800

    SpringBoot应用启动

---
+++ src/main/java/com/xby/SellApplication.java
@@ -0,0 +1,17 @@
+package com.xby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class SellApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SellApplication.class, args);
+    }
+
+}
+
