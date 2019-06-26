043f9d80d86f27666b698b374ec1e8dee11e86ab
summerbegins <2746370543@qq.com>
2019-06-26 15:55:45 +0800

    微信开放平台配置

---
+++ src/main/java/com/xby/config/WechatOpenConfig.java
@@ -0,0 +1,35 @@
+package com.xby.config;
+
+import me.chanjar.weixin.mp.api.WxMpConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author :xby
+ * @date :2019/2/15 13:34
+ * @description :
+ */
+@Component
+public class WechatOpenConfig {
+    @Autowired
+    private WechatAccountConfig accountConfig;
+
+    @Bean
+    public WxMpService wxOpenService() {
+        WxMpService wxOpenService = new WxMpServiceImpl();
+        wxOpenService.setWxMpConfigStorage(wxOpenConfigStorage());
+        return wxOpenService;
+    }
+
+    @Bean
+    public WxMpConfigStorage wxOpenConfigStorage() {
+        WxMpInMemoryConfigStorage wxMpInMemoryConfigStorage = new WxMpInMemoryConfigStorage();
+        wxMpInMemoryConfigStorage.setAppId(accountConfig.getOpenAppId());
+        wxMpInMemoryConfigStorage.setSecret(accountConfig.getOpenAppSecret());
+        return wxMpInMemoryConfigStorage;
+    }
+}
