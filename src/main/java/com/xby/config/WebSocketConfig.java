20bd2a25d32e78f6ddac34da45e91e08ccef4237
summerbegins <2746370543@qq.com>
2019-06-26 16:01:33 +0800

    WebSocket配置

---
+++ src/main/java/com/xby/config/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package com.xby.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author :xby
+ * @date :2019/2/16 22:36
+ * @description :
+ */
+@Component
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
