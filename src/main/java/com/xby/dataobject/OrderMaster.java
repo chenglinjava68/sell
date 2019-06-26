33786bc115a8062cea6ab7d4e835c7822bfebd53
summerbegins <2746370543@qq.com>
2019-06-26 15:01:29 +0800

    订单主体类

---
+++ src/main/java/com/xby/dataobject/OrderMaster.java
@@ -0,0 +1,55 @@
+package com.xby.dataobject;
+
+import com.xby.enums.OrderStatusEnum;
+import com.xby.enums.PayStatusEnum;
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * @author :xby
+ * @date :2019/1/25 11:54
+ * @description :订单
+ */
+@Entity
+@Data
+@DynamicUpdate
+public class OrderMaster {
+
+    /** 订单id. */
+    @Id
+    private String orderId;
+
+    /** 买家名字. */
+    private String buyerName;
+
+    /** 买家手机号. */
+    private String buyerPhone;
+
+    /** 买家地址. */
+    private String buyerAddress;
+
+    /** 买家微信Openid. */
+    private String buyerOpenid;
+
+    /** 订单总金额. */
+    private BigDecimal orderAmount;
+
+    /** 订单状态, 默认为0新下单. */
+    private Integer orderStatus = OrderStatusEnum.NEW.getCode();
+
+    /** 支付状态, 默认为0未支付. */
+    private Integer payStatus = PayStatusEnum.WAIT.getCode();
+
+    /** 创建时间. */
+    private Date createTime;
+
+    /** 更新时间. */
+    private Date updateTime;
+
+}
+
