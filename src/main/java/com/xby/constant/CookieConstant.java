3c94d2d3e1f4e97be3118443c99e0f6def726046
summerbegins <2746370543@qq.com>
2019-06-26 15:58:09 +0800

    Cookie名字、过期时间的配置

---
+++ src/main/java/com/xby/constant/CookieConstant.java
@@ -0,0 +1,14 @@
+package com.xby.constant;
+
+/**
+ * @author :xby
+ * @date :2019/2/15 21:02
+ * @description : cookie常量
+ */
+public interface CookieConstant {
+
+    String TOKEN = "token";
+
+    //设置过期时间2小时(7200秒)
+    Integer EXPIRE = 7200;
+}
