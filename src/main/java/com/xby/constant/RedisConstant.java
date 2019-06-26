ce29319f620d00470ae615c06e788fed965de602
summerbegins <2746370543@qq.com>
2019-06-26 15:58:39 +0800

    Redis名字、过期时间的配置

---
+++ src/main/java/com/xby/constant/RedisConstant.java
@@ -0,0 +1,12 @@
+package com.xby.constant;
+
+/**
+ * @author :xby
+ * @date :2019/2/15 20:47
+ * @description : redis常量
+ */
+public interface RedisConstant {
+
+    String TOKEN_PREFIX = "token_%s";
+    Integer EXPIRE = 7200;//2小时
+}
